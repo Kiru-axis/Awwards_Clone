f7aa5288a1cafde0d6fecd13a404a9878835c331
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-19 09:49:09 +0300

    importing all the model packages required

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# import datetime as dt
+from django.utils import timezone
 # Create your models here.
