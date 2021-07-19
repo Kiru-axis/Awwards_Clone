05fec9b5f867dca1f98bc93658d0e53375739e74
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-19 10:53:50 +0300

    profiles routes and templates

--- awwards/urls.py
+++ awwards/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('', views.index, name='index'),
     path('<username>/profile', views.user_profile, name='userprofile'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
