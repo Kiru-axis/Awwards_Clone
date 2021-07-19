af9de922e279059da750d46e58abb2f8258c3af1
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-19 09:50:04 +0300

    profile model compieted

--- awwards/models.py
+++ awwards/models.py
@@ -4,4 +4,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 # import datetime as dt
 from django.utils import timezone
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="Add Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+
