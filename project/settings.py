aec3bf0015afa199453deeff78c686724ec97fae
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-19 11:24:29 +0300

    route to point to static folder for styling

--- project/settings.py
+++ project/settings.py
@@ -137,11 +137,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
