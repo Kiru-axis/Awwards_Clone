bb8a414fbc77e4fbcbfe1bf86c2b8ff5740871b1
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-19 10:11:05 +0300

    instance and save post unittest

--- awwards/tests.py
+++ awwards/tests.py
@@ -8,8 +8,21 @@ class RatingTest(TestCase):
         self.user = User.objects.create(id=1, username='Test')
         self.post = Post.objects.create(id=1, title='Post', photo='https://cutt.ly/UmBUMAX', description='desc',
                                         user=self.user, url='http://google.com')
-                                        
+
         self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, post=self.post)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = Rating.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+    def test_get_post_rating(self, id):
+        self.rating.save()
+        rating = Rating.get_ratings(post_id=id)
+        self.assertTrue(len(rating) == 1)
 
 
 class ProfileTest(TestCase):
